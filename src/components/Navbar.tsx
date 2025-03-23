54b4c0648edb490e64b409112296862ebde0aacd
HarshK200 <harshkapadia04@gmail.com>
2025-03-23 15:58:25 +0530

    added autoclose sidebar on btn click

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import { ChevronDown, Menu, X } from "lucide-react";
 import { NavActionLink } from "./ui/NavActionBtn";
 import { useState } from "react";
-import { CTABtn } from "./ui/CATBtn";
+import { CTABtn } from "./ui/CTABtn";
 
 export const Navbar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
 
   return (
-    <nav className="border-b-[1px] border-slate-300 py-3 px-4 flex justify-between items-center">
+    <nav className="border-b-[1px] border-slate-300 py-1.5 px-4 flex justify-between items-center">
       <Link to={"/"}>
         <img src="/HSG logo.avif" width={90} />
       </Link>
@@ -60,14 +60,14 @@ const SideBar = ({
         className="absolute top-5 right-4 size-7 cursor-pointer"
         onClick={() => setIsSidebarOpen(false)}
       />
-      <NavActionLink to="/">Home</NavActionLink>
-      <NavActionLink to="/testprep">TestPrep</NavActionLink>
-      <NavActionLink to="/study">Study Destenations</NavActionLink>
-      <NavActionLink to="/scholarships">ScholarShips</NavActionLink>
-      <NavActionLink to="/testimonials">Testimonials</NavActionLink>
-      <NavActionLink to="/blogs">Blogs</NavActionLink>
-      <NavActionLink to="/contact">Contact</NavActionLink>
-      <NavActionLink to="/test" className="flex items-center gap-1">
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/">Home</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/testprep">TestPrep</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/study">Study Destenations</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/scholarships">ScholarShips</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/testimonials">Testimonials</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/blogs">Blogs</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/contact">Contact</NavActionLink>
+      <NavActionLink setIsSidebarOpen={setIsSidebarOpen} to="/test" className="flex items-center gap-1">
         <p>More</p>
         <ChevronDown />
       </NavActionLink>
--- src/components/ui/NavActionBtn.tsx
+++ src/components/ui/NavActionBtn.tsx
@@ -4,12 +4,21 @@ interface buttonProps {
   className?: string;
   children: React.ReactNode;
   to: string;
+  setIsSidebarOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const NavActionLink = ({ children, to, className }: buttonProps) => {
+export const NavActionLink = ({
+  children,
+  to,
+  className,
+  setIsSidebarOpen,
+}: buttonProps) => {
   return (
     <Link
       to={to}
+      onClick={() => {
+        if (setIsSidebarOpen) setIsSidebarOpen(false);
+      }}
       className={`hover:text-cta whitespace-nowrap w-full mx-3 my-3 flex items-start font-normal cursor-pointer ${className}`}
     >
       {children}
